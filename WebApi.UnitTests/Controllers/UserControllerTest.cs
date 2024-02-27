7b5ff040f1423a95caf5486b99c86a10a3c4864a
Willy Arrojas <willyarrojas@gmail.com>
2024-02-27 18:37:35 +0100

    add config test to UserControllerTest

---
+++ WebApi.UnitTests/Controllers/UserControllerTest.cs
@@ -0,0 +1,12 @@
+namespace WebApi.UnitTests;
+
+public class UserControllerTest
+{
+    [Fact]
+    public void Delete_User_Return_Ok()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
