dfcc43959965d91406949b4271917204e29f9ab5
Willy Arrojas <willyarrojas@gmail.com>
2024-02-27 11:45:24 +0100

    add DTO for User

---
+++ Dto/Users/UserResponse.cs
@@ -0,0 +1,15 @@
+namespace RindusBackend.Dto.Users;
+
+public class UserResponse
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? UpdateDate { get; set; }
+
+}
---
+++ Dto/Users/UserUpdateRequest.cs
@@ -0,0 +1,10 @@
+namespace RindusBackend.Dto.Users;
+
+public class UserUpdateRequest
+{
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string? Email { get; set; }
+}
