f0274868e0f856a0f93bb142869618c36bb9c0af
Willy Arrojas <willyarrojas@gmail.com>
2024-02-27 14:41:58 +0100

    configuration connection to Data Base SQL SERVER

--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetKubernetes.Middleware;
+using RindusBackend.Data;
+using RindusBackend.Data.Users;
+using RindusBackend.Profiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"));
+    opt.LogTo(
+        Console.WriteLine,
+        new[] { DbLoggerCategory.Database.Command.Name },
+        LogLevel.Information
+    ).EnableSensitiveDataLogging();
+});
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var mapperConfig = new MapperConfiguration(mc => mc.AddProfile(new UserProfile()));
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddCors(opt => opt.AddPolicy("cors_app",
+    _builder => _builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader()
+));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,8 +40,28 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ManagerMiddleware>();
 
+app.UseCors("cors_app");
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (IServiceScope environment = app.Services.CreateScope())
+{
+
+    IServiceProvider services = environment.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "A migration error has ocurred");
+        throw;
+    }
+}
 
 app.Run();
